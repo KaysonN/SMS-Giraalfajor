395400899f3a145a2ffd4ce280c6917485f245c4
KaysonN <carloskayson@hotmail.com>
2022-07-11 13:45:23 -0300

    Estrutura de backend e frontend criadas

---
+++ backend/src/main/java/com/devkayson/giraalfajor/GiraalfajorApplication.java
@@ -0,0 +1,13 @@
+package com.devkayson.giraalfajor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiraalfajorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiraalfajorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devkayson/giraalfajor/GiraalfajorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkayson.giraalfajor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraalfajorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
