b5b8826dc9bcc5df29059298337a711a1e335bde
KaysonN <carloskayson@hotmail.com>
2022-07-13 12:15:06 -0300

    Configurando e conectando o banco de dados

--- backend/src/main/java/com/devkayson/giraalfajor/entities/Sale.java
+++ backend/src/main/java/com/devkayson/giraalfajor/entities/Sale.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -16,6 +18,7 @@ public class Sale implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//gera ids automaticamente  
 	private Long id;
 	
 	private String sellerName;
