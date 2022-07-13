a0e9303309cf6e4de08568eee34bcb6079c2c672
KaysonN <carloskayson@hotmail.com>
2022-07-13 13:05:17 -0300

    Consulta por data

--- backend/src/main/java/com/devkayson/giraalfajor/services/SaleService.java
+++ backend/src/main/java/com/devkayson/giraalfajor/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devkayson.giraalfajor.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devkayson.giraalfajor.entities.Sale;
@@ -15,8 +19,13 @@ public class SaleService {
 	private SaleRepository repository;
 	
 	// busca todas as vendas
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		//pega a data atual
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		return repository.findSales(min, max, pageable);
 	}
 	
 	
