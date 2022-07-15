c72d3a5ebb96ad88d5a4b0e3ac9a00359f4e87e2
KaysonN <carloskayson@hotmail.com>
2022-07-15 11:07:00 -0300

    Envio de notificação

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
+import axios from 'axios';
 import icon from '../../assets/imagens/notification-icon.svg';
 import '../../index.css';
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id: number){
+    axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
+        console.log("OK");
+    });
+}
+
+function NotificationButton({ saleId }: Props) {
     return (
-        <div className="btn-send">
-            <img src={icon} alt="img"/>
+        <div className="btn-send" onClick={()=> handleClick(saleId)}>
+            <img src={icon} alt="img" />
         </div>
     )
 }
